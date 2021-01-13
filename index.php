32fc2bf113cac7903a4c4af5dff355b22a9146a4
Visinigiri Aditya <adityakumarverified@gmail.com>
2021-01-13 08:44:11 +0530

    added product section with some products

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
         </div>
       </div>
     </section>
-    <div class=" pt-5 pb-5 "></div>
+    <div class=" pt-5 mt-5 mb-5 pb-5 "></div>
     <section>
       <div class="container">
         <div class="row">
@@ -60,17 +60,53 @@
               <h1 class="text-center"> Affliate Companies</h1>
               <p class="text-center">This Associates Program Advertising Fee Schedule (“Schedule”) is part of the Operating Agreement that governs your participation in the Associates Program. This Schedule describes the advertising fee rates you may earn as a participant in the Program. It also describes the limitations that apply to earning advertising fees on certain Products.
 From time to time, we may modify this Schedule in accordance with the Operating Agreement. All capitalized terms used below that are not defined on this page have the meanings given to them in the Operating Agreement.</p>
-<div class="button d-flex justify-content-center ">
-              <a href="store.php" class="btn btn-success">Check Blog</a>
+<div class="button py-2 d-flex justify-content-center ">
+              <a href="blog.php" class="btn btn-success">Check Blog</a>
             </div>
             </div>
             
         </div>
       </div>
     </section>
-    <div class=" pt-5 pb-5 "></div>
+    <div class=""></div>
     <section class="product">
-
+      <h1 class="text-center py-2 pt-4">Product Section</h1>
+    <div class="container">
+         
+         <div class="row">
+           <div class="col-md-10" >
+           <h1 class="text-center">
+            
+          
+         </div>
+         <?php 
+         $sql = "select * from post ";
+       include("dbConnect.php");
+    
+       $result= $pdo->query($sql);
+       
+     $rs =   $result->fetchAll();
+      
+     foreach($rs as $row){
+ ?> 
+ 
+        <!-- Card Start -->
+          <div class="col-md-3 p-3">
+                <div class="card" style="width: 18rem;">
+                  <img class="card-img-top" src="uploads/<?php echo $row['image'];?>" alt="shinzo" width="100%" height="300px">
+                  <hr>
+                  <div class="card-body">
+                    <h3 class="card-title"><?php echo $row['title']; ?></h3>
+                    
+                    <hr>
+                   <h5 class="card-text pb-2">₹ <?php echo $row['price']; ?></h5>
+                   <a href=<?php echo $row['link']; ?>  class="btn btn-danger">Features</a>
+                    <a href=<?php echo $row['link']; ?>  class="btn btn-primary">Book Now</a>
+                    
+                   </div>
+                   </div>
+                </div>
+              <?php } ?>
     </section>
     <!-- bootstrap -->
     <script
