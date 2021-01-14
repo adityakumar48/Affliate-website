67938e58b141950181e80fcffa80c0da983f439a
Visinigiri Aditya <adityakumarverified@gmail.com>
2021-01-14 17:08:55 +0530

    modify

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -1,6 +1,19 @@
 <?php
 include('AdminHeader.html')
 ?>
+<?php
+include('dbConnect.php');
+  $sql = "update visitor set visitor=visitor+1";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+  
+  $sql = "select visitor from visitor";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute();
+  $arr=$stmt->fetchAll(PDO::FETCH_ASSOC);
+  $counter =  $arr[0] ['visitor'];
+  $count=strlen($counter);
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -15,12 +28,36 @@ include('AdminHeader.html')
       crossorigin="anonymous"
     />
     <!-- bootstrap -->
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="css/AdminDashboard.css">
+    <!-- Custom Css -->
   </head>
   <body>
-    <a href="new-post.html">New Post</a>
-    <a href="Show-posts.php">All Posts</a>
-    <a href="Store.php">Store</a>
-
+    <hr>
+<div class="pt-5 pb-2">
+<section class="text-center mt-5 mb-5 pb-5 pt-5 mx-auto d-flex align-items-center justify-content-center" >
+<div class="container ">
+  <div class="row">
+    <div class="col-md-12  ">
+      <h2> Page Visitor </h2>
+          <div class="visitorCounter pt-2 offset-md-2 text-center ">
+            <div class="offset-md-4 text-center">
+            <ul id="visitor">
+              <?php for($i=0;$i<$count;$i++) {?>
+              <li><?php echo $counter[$i]?></li>
+                <?php } ?>
+            </ul>
+            </div>
+          </div>
+          </div>
+    </div>
+  </div>
+</div>
+</section>
+</div>
+<?php
+include('footer.html')
+?>
     <!-- bootstrap -->
     <script
       src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
@@ -37,6 +74,10 @@ include('AdminHeader.html')
       integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
       crossorigin="anonymous"
     ></script>
+    
     <!-- bootstrap -->
+
+ 
+
   </body>
 </html>
